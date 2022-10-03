fe5f224a734623631876dd376db2f390c8b3abe5
Dhammafarer <dhammafarer@tuta.io>
2022-10-03 14:28:39 +0800

    add dmenu-bookmarks

---
+++ dmenu/dmenu-bookmarks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+conf=/home/pawel/.config/gtk-3.0/bookmarks
+
+#[[ ! -e "$conf" ]] && echo "ERROR: $conf does not exist"; exit 1;
+
+opts=$(cat $conf)
+
+# Get the file choice
+choice=$(echo "$opts" | awk -F '/' '{print $NF}'| mydmenu -sb "#5294e2" -p 'Open bookmark:')
+
+# If selection is empty, exit
+[[ -z "$choice" ]] && { exit 1; }
+
+# Extract a path based on the choice
+path=$(echo "$opts" | awk -F '/' -v re="$choice" '$NF == re {print $0}')
+
+xdg-open $path
