03c0da4ec1335de6848fdc0c178e654b1a64a82a
Pawel Lisewski <pawel.lisewski@tuta.io>
2022-10-04 18:48:19 +0800

    update

--- dmenu/dmenu-bookmarks.sh
+++ dmenu/dmenu-bookmarks.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 # File to source
-$sb="#FFA686"
 conf=$1
 
 [[ ! -e "$conf" ]] && touch $1;
@@ -9,7 +8,7 @@ conf=$1
 opts=$(cat $conf)
 
 # Get the file choice
-choice=$(echo "$opts" | sort -k 1 -nr | awk '{print $2}'| mydmenu -i -sb "#FFA686" -p "Bookmarks:")
+choice=$(echo "$opts" | sort -k 1 -nr | awk '{print $2}'| mydmenu -i -sb "#ef8354" -p "Bookmarks:")
 
 # If selection is empty, exit
 [[ -z "$choice" ]] && { exit 1; }
@@ -17,16 +16,16 @@ choice=$(echo "$opts" | sort -k 1 -nr | awk '{print $2}'| mydmenu -i -sb "#FFA68
 case "$choice" in
   "+")
     # Get the label
-    label=$(mydmenu -p "Label:")
+    label=$(mydmenu -sb "#ef8354" -p "Label:")
 
     # Get the url
-    url=$(mydmenu -p "URL:")
+    url=$(mydmenu -sb "#ef8354"  -p "URL:")
 
     # Exit if no label or url
     [[ -z "$label" || -z "$url" ]] && { notify-send -u critical "Bookmark Error" "Label or URL are empty."; exit 1; }
 
     # Get the tags
-    tags=$(mydmenu -p "Tags:" | tr " " ":")
+    tags=$(mydmenu -sb "#ef8354" -p "Tags:" | tr " " ":")
     [[ -z "$tags" ]] && tags="none"
 
     # append to the conf file
@@ -41,9 +40,9 @@ case "$choice" in
 	  myedit $conf
 	;;
   "#")
-    tag=$(echo "$opts" | awk '{print $4}'| tr ":" "\n" | sort | uniq | mydmenu -i -sb "#FFA686" -p "Tags:")
+    tag=$(echo "$opts" | awk '{print $4}'| tr ":" "\n" | sort | uniq | mydmenu -i -sb "#ef8354" -p "Tags:")
 
-    tagged=$(echo "$opts" | awk -v re="$tag" 'match($4, re) {print $2}' | mydmenu -i -sb "#FFA686" -p "$tag:")
+    tagged=$(echo "$opts" | awk -v re="$tag" 'match($4, re) {print $2}' | mydmenu -i -sb "#ef8354" -p "$tag:")
 
     url=$(echo "$opts" | awk -v re="$tagged" '$2 == re {print $3}')
 
@@ -52,6 +51,7 @@ case "$choice" in
 	*)
     url=$(echo "$opts" | awk -v re="$choice" '$2 == re {print $3}')
 
+    [[ -z "$url" ]] && { exit 1; }
     firefox $url
   ;;
 esac
