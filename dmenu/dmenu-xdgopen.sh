38a49ff2bf22250224e3c6585089f1fda123821c
Pawel Lisewski <pawel.lisewski@tuta.io>
2022-10-06 18:22:35 +0800

    updates

--- dmenu/dmenu-xdgopen.sh
+++ dmenu/dmenu-xdgopen.sh
@@ -2,16 +2,18 @@
 
 dir=$1
 prompt=$2
+launcher="/$HOME/.local/bin/rofi -dmenu -i"
+#launcher="mydmenu -i -sb #5294e2"
 
 opts=$(ls $1)
 
 # Get the file choice
-choice=$(echo "$opts" | awk -F '.' '{print $1}'| mydmenu -i -sb "#5294e2" -p "$prompt")
-
-# If selection is empty, exit
-[[ -z "$choice" ]] && { exit 1; }
+choice=$(echo "$opts" | awk -F '.' '{print $1}'| $launcher -p "$prompt")
 
 # Extract a path based on the choice
 filename=$(echo "$opts" | awk -F '.' -v re="$choice" '$1 == re {print $0}')
 
-xdg-open $dir/$filename
+if [[ "$filename" ]]; then
+  xdg-open $dir/$filename
+fi
+
