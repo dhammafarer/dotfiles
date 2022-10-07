551c2df6008ebd7167803c89d8a057cadbc3be22
Pawel Lisewski <pawel.lisewski@tuta.io>
2022-10-07 16:37:16 +0800

    fix row.rasi

--- dmenu/dmenu_quit.sh
+++ dmenu/dmenu_quit.sh
@@ -4,9 +4,12 @@ launcher="rofi -dmenu -i -config $HOME/dotfiles/config/rofi/row.config.rasi"
 
 poweroff=""
 reboot=""
+log_out=""
+
 declare -a options=(
 "$poweroff"
 "$reboot"
+"$log_out"
 )
 
 # The combination of echo and printf is done to add line breaks to the end of each
@@ -16,14 +19,19 @@ declare -a options=(
 choice=$(echo -e "$(printf '%s\n' "${options[@]}")" | $launcher -p 'Restart process: ')
 case "$choice" in
 	"$reboot")
-    confirm=$(echo -e "yes\nno" | mydmenu -sb "#da6371" -sf "#2f343f" -p "Reboot?")
+    confirm=$(echo -e "yes\nno" | mydmenu -sb "#da6371" -sf "#2f343f" -p "Confirm")
 
     [[ "$confirm" == "yes" ]] && { systemctl reboot; }
 	;;
 	"$poweroff")
-    confirm=$(echo -e "yes\nno" | mydmenu -sb "#da6371" -sf "#2f343f" -p "Power off?")
+    confirm=$(echo -e "yes\nno" | mydmenu -sb "#da6371" -sf "#2f343f" -p "Conifrm")
 
     [[ "$confirm" == "yes" ]] && { systemctl poweroff; }
+	;;
+	"$log_out")
+    confirm=$(echo -e "yes\nno" | mydmenu -sb "#da6371" -sf "#2f343f" -p "Confirm")
+
+	  sleep 0.1; xdotool key super+shift+m
 	;;
 	*)
 		exit 1
