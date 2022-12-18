d58e8c7aec756f89047b33f2f27e84e0cb50d9e8
Pawel Lisewski <pawel.lisewski@tuta.io>
2022-12-18 15:35:05 +0800

    dmenu_xrandr update

--- dmenu/dmenu_xrandr.sh
+++ dmenu/dmenu_xrandr.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
-declare -a options=(
-"ctn:asus"
-"ctn:dual"
-"ctn:huion"
-"nuc:asus"
-"nuc:dual"
-"nuc:huion"
-"nuc:tv"
+declare -a options_ctn=(
+"asus"
+"dual"
+"huion"
+)
+
+declare -a options_nuc=(
+"asus"
+"dual"
+"huion"
+"tv"
 )
 
 ctn_asus=DP-5
@@ -17,42 +20,55 @@ nuc_tv=HDMI-A-0
 nuc_asus=HDMI-A-1
 nuc_huion=DisplayPort-0
 
-# The combination of echo and printf is done to add line breaks to the end of each
-# item in the array before it is piped into dmenu.  Otherwise, all the items are listed
-# as one long line (one item).
-
-choice=$(echo "$(printf '%s\n' "${options[@]}")" | mydmenu -sb "#da6371" -sf "#2f343f" -p 'Restart process: ')
-case "$choice" in
-	ctn:asus)
-    xrandr --output $ctn_asus --auto --primary --output $ctn_huion --off
-	;;
-	ctn:huion)
-    xrandr --output $ctn_huion --auto --primary --output $ctn_asus --off
-	;;
-	ctn:dual)
-    xrandr --output $ctn_asus --auto --primary --output $ctn_huion --auto --below DP-5
-	;;
-	nuc:asus)
-    xrandr --output $nuc_asus --auto --primary \
-      --output $nuc_huion --off \
-      --output $nuc_tv --off
-	;;
-	nuc:huion)
-    xrandr --output $nuc_asus  --off \
-      --output $nuc_huion --auto --primary \
-      --output $nuc_tv --off
-	;;
-	nuc:dual)
-    xrandr --output $nuc_asus --auto --primary \
-      --output $nuc_huion --auto --below $nuc_asus \
-      --output $nuc_tv --off
-	;;
-	nuc:tv)
-    xrandr --output $nuc_asus --auto --primary \
-      --output $nuc_huion --off \
-      --output $nuc_tv --mode 1360x768 --right-of $nuc_asus
-	;;
-	*)
-		exit 1
-	;;
-esac
+if [[ $(hostname) == "debian-nuc" ]];
+then
+  choice=$(echo "$(printf '%s\n' "${options_nuc[@]}")" | mydmenu -sb "#5294e2" -sf "#2f343f" -p 'xrandr profile: ')
+  case "$choice" in
+    asus)
+      xrandr --output $nuc_asus --auto --primary \
+        --output $nuc_huion --off \
+        --output $nuc_tv --off
+    ;;
+    huion)
+      xrandr --output $nuc_asus  --off \
+        --output $nuc_huion --auto --primary \
+        --output $nuc_tv --off
+    ;;
+    dual)
+      xrandr --output $nuc_asus --auto --primary \
+        --output $nuc_huion --auto --below $nuc_asus \
+        --output $nuc_tv --off
+    ;;
+    tv)
+      xrandr --output $nuc_asus --off \
+        --output $nuc_huion --off \
+        --output $nuc_tv --mode 1360x768 --primary
+    ;;
+    *)
+      exit 1
+    ;;
+  esac
+elif [[ $(hostname) == "ctn" ]];
+then
+  choice=$(echo "$(printf '%s\n' "${options_ctn[@]}")" | mydmenu -sb "#5294e2" -sf "#2f343f" -p 'xrandr profile: ')
+  case "$choice" in
+    asus)
+      xrandr --output $ctn_asus --auto --primary \
+        --output $ctn_huion --off
+    ;;
+    huion)
+      xrandr --output $ctn_huion --auto --primary \
+        --output $ctn_asus --off
+    ;;
+    dual)
+      xrandr --output $ctn_asus --auto --primary --output \
+        $ctn_huion --auto --below DP-5
+    ;;
+    *)
+      exit 1
+    ;;
+  esac
+else
+  notify-send "xrandr unavailable on this host"
+  exit 1
+fi
