4b9df58b4b3e94fe538eb64f6a02b139d091c946
Pawel Lisewski <pawel.lisewski@tuta.io>
2023-02-08 11:27:53 +0800

    add .desktop files

--- desktop/blender.sh
+++ desktop/blender.sh
@@ -3,4 +3,4 @@
 /usr/bin/sxhkd -c /home/pl/.config/sxhkd/blender.sxhkdrc /home/pl/.config/sxhkd/sxhkdrc &
 notify-send "Blender mappings"
 
-/home/pl/Apps/blender-current/blender $@
+/home/pl/Apps/blender/blender $@
