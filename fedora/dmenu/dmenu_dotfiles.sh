fb3e7600080f3cd7f8d940639616fa73ab6a866c
Pale Kiwi <contact@palekiwi.com>
2023-04-14 15:38:49 +0800

    fix fedora dmenu

--- fedora/dmenu/dmenu_dotfiles.sh
+++ fedora/dmenu/dmenu_dotfiles.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-dotfile=$1
+file=~/.dotfiles
 launcher="mydmenu -sb #ffb05f -i -b"
 
-opts=$(cat $dotfile)
+opts=$(cat $file)
 # Get the file choice
 choice=$(echo "$opts" | awk '{print $1}'| $launcher -p 'Edit config file: ')
 
@@ -14,5 +14,5 @@ choice=$(echo "$opts" | awk '{print $1}'| $launcher -p 'Edit config file: ')
 path=$(echo "$opts" | awk -v re="$choice" '$1 == re {print $2}')
 
 # execute command with path
-#xfce4-terminal -e "distrobox enter arch -e 'nvim $path'" -T "nvim $path"
-myedit $path
+xfce4-terminal -e "distrobox enter dev -e 'nvim $path'" -T "nvim $path"
+#myedit $path
