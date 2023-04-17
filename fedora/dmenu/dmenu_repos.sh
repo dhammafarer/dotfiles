822dde8b8875c9f0619226d2d344cb1fb32586c4
Pale Kiwi <contact@palekiwi.com>
2023-04-17 17:20:49 +0800

    change fedora/dmenu_repos

--- fedora/dmenu/dmenu_repos.sh
+++ fedora/dmenu/dmenu_repos.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/bash
 
-conf="$HOME/.repos"
+dir="$HOME/.repos"
 
 #[[ ! -e "$conf" ]] && echo "ERROR: $conf does not exist"; exit 1;
 
-opts=$(cat $conf)
+opts=$(ls $dir)
 
 launcher='dmenu -i -nb #192330 -nf #D3D7CF -sb #e96a9d -sf #192330 -fn 11'
 
 # Get the file choice
-choice=$(echo "$opts" | awk -F '/' '{print $NF}'| $launcher -p "Open repo:")
+#choice=$(echo "$opts" | awk -F '/' '{print $NF}'| $launcher -p "Open repo:")
+choice=$(echo "$opts" | $launcher -p "Open repo:")
 
 # If selection is empty, exit
 [[ -z "$choice" ]] && { exit 1; }
 
 # Extract a path based on the choice
-path=$(echo "$opts" | sed 's/file:\/\///'  |awk -F '/' -v re="$choice" '$NF == re {print $0}')
+#path=$(echo "$opts" | sed 's/file:\/\///'  |awk -F '/' -v re="$choice" '$NF == re {print $0}')
+
+path="$dir/$choice"
 
 xfce4-terminal -e "distrobox enter dev -e 'gitui'" --default-working-directory=$path
