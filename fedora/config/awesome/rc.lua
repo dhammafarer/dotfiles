766799a0c851f6ef40445aaa8171a9aee7947798
Pale Kiwi <contact@palekiwi.com>
2023-04-18 16:05:40 +0800

    add clientbuttons

--- fedora/config/awesome/rc.lua
+++ fedora/config/awesome/rc.lua
@@ -220,20 +220,7 @@ root.buttons(gears.table.join(
 
 globalkeys = require("globalkeys")
 clientkeys = require("clientkeys")
-
-clientbuttons = gears.table.join(
-    awful.button({ }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-    end),
-    awful.button({ modkey }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.move(c)
-    end),
-    awful.button({ modkey }, 3, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.resize(c)
-    end)
-)
+clientbuttons = require("clientbuttons")
 
 root.keys(globalkeys)
 
