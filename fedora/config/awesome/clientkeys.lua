c354cdb208dba7743c43552b087a54a456ed8ad9
Pale Kiwi <contact@palekiwi.com>
2023-04-18 15:26:43 +0800

    add clientkeys

---
+++ fedora/config/awesome/clientkeys.lua
@@ -0,0 +1,18 @@
+local gears = require("gears")
+local awful = require("awful")
+
+clientkeys = gears.table.join(
+  -- Next Layout
+  awful.key({ modkey, "Control" }, "s",
+    function (c) c.fullscreen = not c.fullscreen; c:raise() end,
+    {description = "toggle fullscreen", group = "client"}
+  ),
+
+  -- Close Client
+  awful.key({ modkey }, "q", function (c) c:kill() end, {description = "close", group = "client"}),
+
+  -- Toggle floating
+  awful.key({ modkey, "Control" }, "z",  awful.client.floating.toggle , {description = "toggle floating", group = "client"})
+)
+
+return clientkeys
