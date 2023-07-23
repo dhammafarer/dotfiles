0c843313afd0f65622d9a15f76d0f9d70e79efdd
Pale Kiwi <contact@palekiwi.com>
2023-07-23 22:47:37 +0800

    updates

--- fedora/config/awesome/nuc.autostart.sh
+++ fedora/config/awesome/nuc.autostart.sh
@@ -14,6 +14,13 @@ function run_flatpak {
   fi
 }
 
+function run_app {
+  if ! pgrep $1;
+  then
+    ~/apps/$@&
+  fi
+}
+
 run picom -b --config $HOME/.config/picom/picom.conf
 run sxhkd
 run ibus-daemon -drxR
@@ -22,5 +29,7 @@ run_flatpak com.nextcloud.desktopclient.nextcloud
 run_flatpak org.signal.Signal
 
 xmodmap ~/.xmodmap
+
+run_app Logseq
 
 run xscreensaver -no-splash
