119a9ab71041f924f34b41f3a9fe38ca9a15163f
Pale Kiwi <contact@palekiwi.com>
2023-12-05 18:02:57 +0800

    update deck.autostart.sh

--- fedora/config/awesome/deck.autostart.sh
+++ fedora/config/awesome/deck.autostart.sh
@@ -28,15 +28,17 @@ function run_app {
   fi
 }
 
-export SXHKDRC="$HOME/.config/sxhkd/dmenu.sxhkdrc $HOME/.config/sxhkd/sxhkdrc"
-
 xrandr --output eDP --rotate right
+
+export SXHKDRC="$HOME/.config/sxhkd/dmenu.sxhkdrc $HOME/.config/sxhkd/sxhkdrc"
 
 run picom -b --config $HOME/.config/picom/picom.conf
 run sxhkd -c $SXHKDRC
 run ibus-daemon -drxR
+run unclutter --timeout 1 --start-hidden --ignore-scrolling
+
+run_flatpak org.signal.Signal
 
 xmodmap ~/.xmodmap
 
 run xscreensaver -no-splash
-
