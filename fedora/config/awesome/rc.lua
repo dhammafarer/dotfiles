a49a542952d14e8a9752a096b959fd67b7b772cc
Pale Kiwi <contact@palekiwi.com>
2023-12-21 11:59:25 +0800

    [micky] add widgets/micky

--- fedora/config/awesome/rc.lua
+++ fedora/config/awesome/rc.lua
@@ -7,6 +7,8 @@ local naughty = require("naughty")
 
 require("awful.autofocus")
 
+require('widgets/micky')
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
