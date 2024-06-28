9e85647e656d4fe35e8f6938e8e9a0e555569d1d
Pawel Lisewski <dev@palekiwi.com>
2024-06-28 18:54:36 +0800

    create snippets/markdown

---
+++ arch/config/nvim/snippets/markdown/pr.lua
@@ -0,0 +1,103 @@
+local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
+local f = ls.function_node
+local c = ls.choice_node
+local sn = ls.snippet_node
+
+-- variables
+local repos = { t("spabreaks"), t("sales"), t("sb-voucher-redemptions") }
+local base_card_url = "https://palatinategroup.atlassian.net/browse/"
+--
+
+-- patterns
+local phrase = "^%s*(.-)%s*$"
+local pr = "#[%d-]+"
+local card = "%[[%a-%d]+%]"
+
+-- functions
+
+local cur_file = function()
+    return vim.fn.expand('%:t:r')
+end
+
+local clipboard = function()
+    return vim.fn.getreg("+") .. "";
+end
+
+local match_pr_id = function()
+    local match = string.match(clipboard(), pr)
+    if match then
+        return string.sub(match, 2)
+    else
+        return cur_file
+    end
+end
+
+local match_cards = function()
+    local cards = {}
+    for w in string.gmatch(clipboard(), card) do
+        table.insert(cards, string.sub(w, 2, -2))
+    end
+    return cards
+end
+
+local card_urls = function()
+    local res = {}
+    for _, v in ipairs(match_cards()) do
+        table.insert(res, base_card_url .. v)
+    end
+
+    return table.concat(res, " ")
+end
+
+local cards = function()
+    return table.concat(match_cards(), "-")
+end
+
+local match_title = function()
+    return string.match(
+        string.gsub(string.gsub(clipboard(), card, ""), pr, ""),
+        phrase
+    )
+end
+
+local branch_name = function()
+    local res = {}
+    for w in string.gmatch(clipboard(), card) do
+        table.insert(res, string.sub(w, 2, -2))
+    end
+
+    local t = string.gsub(string.lower(match_title()), "%s", "-")
+
+    return table.concat(res, "-") .. "-" .. t
+end
+
+local repo_choice = sn(1, { c(1, repos) })
+
+return {
+    s("_template:pr", fmt(
+        [[
+        ---
+        id: {}
+        title: {}
+        url: https://github.com/ygt/{}/pull/{}
+        card: {}
+        branch: {}
+        ---
+        ]],
+        {
+            f(match_pr_id),
+            f(match_title),
+            repo_choice,
+            f(match_pr_id),
+            f(card_urls),
+            f(branch_name)
+        })
+    ),
+    s("new_pr", fmt(
+        [[
+        [{} {}](./{}.md)
+        ]],
+        { f(cards), f(match_title), f(match_pr_id) }
+    ))
+}
