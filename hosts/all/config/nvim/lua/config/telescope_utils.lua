e3868f84a0f32d127cbb3d0c96ea9462e81ab032
Pawel Lisewski <dev@palekiwi.com>
2024-11-11 17:59:28 +0800

    Add displayer

--- hosts/all/config/nvim/lua/config/telescope_utils.lua
+++ hosts/all/config/nvim/lua/config/telescope_utils.lua
@@ -5,6 +5,7 @@ local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 local previewers = require "telescope.previewers"
 local make_entry = require "telescope.make_entry"
+local entry_display = require "telescope.pickers.entry_display"
 local gs = require('gitsigns')
 
 M = {}
@@ -12,7 +13,11 @@ M = {}
 M.changed_files = function(opts)
 	local base_branch = vim.g.git_base or "master"
 	local command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )"
+    -- git diff --name-status HEAD ^master | grep -v '^D'
+
 	local handle = io.popen(command)
+    if handle == nil then return end
+
 	local result = handle:read("*a")
 	handle:close()
 
@@ -32,12 +37,53 @@ M.changed_files = function(opts)
 	}):find()
 end
 
+local gen_from_git_commits = function (opts)
+  opts = opts or {}
+
+  local displayer = entry_display.create {
+    separator = " ",
+    items = {
+      { width = 8 },
+      { width = 10 },
+      { remaining = true },
+    },
+  }
+
+  local make_display = function(entry)
+    return displayer {
+      { entry.value, "TelescopeResultsIdentifier" },
+      entry.date,
+      entry.msg,
+    }
+  end
+
+  return function(entry)
+    if entry == "" then
+      return nil
+    end
+
+    local sha, date, msg = string.match(entry, "([^ ]+) ([^ ]+) (.+)")
+
+    return make_entry.set_default_entry_mt({
+      value = sha,
+      ordinal = sha .. " " .. date .. " " .. msg,
+      date = date,
+      msg = msg,
+      display = make_display,
+      current_file = opts.current_file,
+    }, opts)
+  end
+end
+
+
 M.git_commits = function(opts)
 	local base_branch = vim.g.git_base or "master"
 	--local command = "git log --pretty=format:'%h %as %<(16)%an %s' HEAD ^" .. base_branch
 	local command = "git log --pretty=format:'%h %as %<(16)%an %s' HEAD ^" .. base_branch
 
 	local handle = io.popen(command)
+    if handle == nil then return end
+
 	local result = handle:read("*a")
 	handle:close()
 
@@ -63,14 +109,15 @@ M.git_commits = function(opts)
 		prompt_title = "PR Commits",
 		finder = finders.new_table {
 			results = files,
-            entry_maker = function(line)
-              print(line)
-              return make_entry.set_default_entry_mt({
-                value = string.match(line, "[^%s]+"),
-                ordinal = line,
-                display = line,
-              }, {})
-      end,
+            entry_maker = gen_from_git_commits(opts),
+            -- entry_maker = function(line)
+            --   print(line)
+            --   return make_entry.set_default_entry_mt({
+            --     value = string.match(line, "[^%s]+"),
+            --     ordinal = line,
+            --     display = line,
+            --   }, {})
+            -- end,
 		},
         previewer = {
           previewers.git_commit_diff_to_parent.new(opts),
