7c6de455befe8b4e396fbd993cdd54bdabfcf134
Pawel Lisewski <dev@palekiwi.com>
2024-11-13 18:00:20 +0800

    Change tmux mappings

--- hosts/all/home-manager/common/programs/tmux.nix
+++ hosts/all/home-manager/common/programs/tmux.nix
@@ -6,7 +6,7 @@
     extraConfig = builtins.readFile ../../../config/tmux/tmux.conf;
     keyMode = "vi";
     mouse = true;
-    prefix = "M-e";
+    prefix = "M-y";
     plugins = [
       pkgs.tmuxPlugins.sensible
       pkgs.tmuxPlugins.yank
