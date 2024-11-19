3185e2771f0d15850d08887398101ab06d172dba
Pawel Lisewski <dev@palekiwi.com>
2024-11-19 19:21:49 +0800

    Formatting

--- hosts/all/config/nvim/lua/config/utils/rails.lua
+++ hosts/all/config/nvim/lua/config/utils/rails.lua
@@ -1,30 +1,28 @@
-local telescope_builtin = require'telescope.builtin'
+local telescope_builtin = require 'telescope.builtin'
 
 M = {}
 
 M.find_template_render = function()
-    local filename = vim.fn.expand("%:t:r:r"):gsub("^_", "")
-    local regex = "(render|partial:)[\\s(]?[\'\"][^\\s]*" .. filename .. "[\'\"]\\B"
+  local filename = vim.fn.expand("%:t:r:r"):gsub("^_", "")
+  local regex = "(render|partial:)[\\s(]?[\'\"][^\\s]*" .. filename .. "[\'\"]\\B"
 
-    telescope_builtin.grep_string(
-        {
-            search_dirs = { "app" },
-            search = regex,
-            use_regex=true
-        }
-    )
+  telescope_builtin.grep_string(
+    {
+      search_dirs = { "app" },
+      search = regex,
+      use_regex = true
+    }
+  )
 end
 
 M.find_template = function()
-  telescope_builtin.find_files({
-    search_dirs = { "app/views" },
-    search_file = vim.fn.expand(
-      "<cword>")
-  })
+  telescope_builtin.find_files(
+    {
+      search_dirs = { "app/views" },
+      search_file = vim.fn.expand(
+        "<cword>")
+    }
+  )
 end
-
--- local find_spec = function()
---     require'telescope.builtin'.find_files({search_dirs = { "spec/" }, search_file = vim.fn.expand("%:t:r") })
--- end
 
 return M
