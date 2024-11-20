fcfd8e9dcc24c53d2ccf6549e1f42bf599c112ab
Pawel Lisewski <dev@palekiwi.com>
2024-11-20 19:07:05 +0800

    Update mappings and git

--- hosts/all/config/nvim/lua/config/utils/git.lua
+++ hosts/all/config/nvim/lua/config/utils/git.lua
@@ -1,10 +1,32 @@
 local gs = require('gitsigns')
+local nt = require('neo-tree.command')
 
 M = {}
 
-M.hunks_to_loclist = function()
-  gs.setqflist("attached", { use_location_list = true, open = true })
+local function toggle_git_tree(action, toggle)
+  nt.execute({
+    action = action,
+    position = "right",
+    toggle = toggle,
+    source = "git_status",
+    git_base = vim.g.git_base
+  })
 end
+
+local function set_base_branch(branch)
+  local base_branch = branch or "master"
+
+  vim.g.git_base = base_branch
+
+  gs.change_base(base_branch, true)
+  toggle_git_tree("show", false)
+
+  vim.notify("Base branch: " .. base_branch)
+end
+
+M.set_base_branch = function(branch) set_base_branch(branch) end
+
+M.toggle_git_tree = function() toggle_git_tree("show", true) end
 
 M.toggle_git_status = function(action, toggle, float, position, git_base)
   if git_base == nil then
@@ -21,19 +43,11 @@ M.toggle_git_status = function(action, toggle, float, position, git_base)
   })
 end
 
-M.set_base_branch = function(git_base, action)
-  if git_base == nil then
-    git_base = vim.g.git_base
-  else
-    vim.g.git_base = git_base
-  end
-
-  gs.change_base(git_base, true)
-
-  M.toggle_git_status(action, false, true, "float", git_base)
-end
-
 M.prev_hunk = function() gs.nav_hunk("prev", { preview = false, wrap = true }) end
 M.next_hunk = function() gs.nav_hunk("next", { preview = false, wrap = true }) end
+
+M.hunks_to_loclist = function()
+  gs.setqflist("attached", { use_location_list = true, open = true })
+end
 
 return M
