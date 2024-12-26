83f6e1258ea81ea6047ade83cdf64439356eebb1
Pawel Lisewski <dev@palekiwi.com>
2024-12-26 16:01:35 +0800

    Rewrite dmenu_tmux

--- hosts/all/scripts/dmenu/dmenu_tmux.sh
+++ hosts/all/scripts/dmenu/dmenu_tmux.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-launcher='dmenu -i -nb #1d1f21 -nf #D3D7CF -sb #37ADD4 -sf #192330 -fn 11'
 launcher="rofi -dmenu -i"
 
-# options=$(tmux list-sessions -F '#S')
-options=$(sesh list)
+options=$(sesh list --json | jq -r '.[] | .Name + "," + .Src' | column -s"," -t) 
 
 choice=$(echo "$(printf '%s\n' "${options[@]}")" | $launcher -p 'Tmux sessions')
 
 [[ -z "$choice" ]] && { exit 1; }
 
-kitty -T $choice -e sesh connect $choice 
+session_name=$(echo "$choice" | cut -d' ' -f1)
+
+kitty -T $session_name -e sesh connect $session_name
