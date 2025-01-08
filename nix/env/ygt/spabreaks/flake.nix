fdbe5cc4d99f5a817407d3f92d2816ede1a497a0
Pawel Lisewski <dev@palekiwi.com>
2025-01-08 14:12:28 +0800

    Change the flake in ygt-env/spabreaks

--- nix/env/ygt/spabreaks/flake.nix
+++ nix/env/ygt/spabreaks/flake.nix
@@ -1,27 +1,84 @@
 {
-  description = "Spabreaks Dev Shell";
+  description = "Spabreaks";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    sqlfluff_0_8_2.url = "github:nixos/nixpkgs?rev=61d24cba72831201efcab419f19b947cf63a2d61";
+    nixpkgs.url = "nixpkgs";
+    ruby-nix.url = "github:inscapist/ruby-nix";
+    # a fork that supports platform dependant gem
+    bundix = {
+      url = "github:inscapist/bundix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fu.url = "github:numtide/flake-utils";
+    bob-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+    bob-ruby.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      sqlfluff = inputs.sqlfluff_0_8_2.legacyPackages.${system}.sqlfluff;
-      ruby = pkgs.ruby_3_3;
-
-      gems = pkgs.bundlerEnv {
-        name = "spabreaks-bundler-env";
-        inherit ruby;
-        gemfile = ./Gemfile;
-        lockfile = ./Gemfile.lock;
-        gemset = ./gemset.nix;
-      };
-    in
+  outputs =
     {
-      devShells.x86_64-linux.default = import ./shell.nix { inherit pkgs; };
-    };
+      self,
+      nixpkgs,
+      fu,
+      ruby-nix,
+      bundix,
+      bob-ruby,
+    }:
+    with fu.lib;
+    eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ bob-ruby.overlays.default ];
+        };
+        rubyNix = ruby-nix.lib pkgs;
+
+        # TODO generate gemset.nix with bundix
+        gemset = if builtins.pathExists ./gemset.nix then import ./gemset.nix else { };
+
+        # If you want to override gem build config, see
+        #   https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/ruby-modules/gem-config/default.nix
+        gemConfig = { };
+
+        # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
+        ruby = pkgs."ruby-3.3.6";
+
+        # Running bundix would regenerate `gemset.nix`
+        bundixcli = bundix.packages.${system}.default;
+
+        # Use these instead of the original `bundle <mutate>` commands
+        bundleLock = pkgs.writeShellScriptBin "bundle-lock" ''
+          export BUNDLE_PATH=vendor/bundle
+          bundle lock
+        '';
+        bundleUpdate = pkgs.writeShellScriptBin "bundle-update" ''
+          export BUNDLE_PATH=vendor/bundle
+          bundle lock --update
+        '';
+      in
+      rec {
+        inherit
+          (rubyNix {
+            inherit gemset ruby;
+            name = "spabreaks";
+            gemConfig = pkgs.defaultGemConfig // gemConfig;
+          })
+          env
+          ;
+
+        devShells = rec {
+          default = dev;
+          dev = pkgs.mkShell {
+            name = "spabreaks";
+            buildInputs =
+              [
+                env
+                bundixcli
+                bundleLock
+                bundleUpdate
+              ];
+          };
+        };
+      }
+    );
 }
