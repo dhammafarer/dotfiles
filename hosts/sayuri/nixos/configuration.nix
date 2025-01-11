b1ecd8ab7892b87682a34ed29733ee4a7ba7a3c1
Pawel Lisewski <dev@palekiwi.com>
2025-01-11 16:18:33 +0800

    Disable gd tty console on startup

--- hosts/sayuri/nixos/configuration.nix
+++ hosts/sayuri/nixos/configuration.nix
@@ -171,6 +171,10 @@
     };
   };
 
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
   services.displayManager.defaultSession = "none+awesome";
 
   services.openssh = {
